8b343b345e3abfde05fbde8dc15b8103ab0996d5
copa-los-de-solanda <cfm.10c.nicolasparedes@gmail.com>
2025-07-12 13:57:41 -0500

    cicd

--- src/entradas/dto/update_entradas.ts
+++ src/entradas/dto/update_entradas.ts
@@ -1,24 +1,30 @@
 import { Type } from 'class-transformer';
-import { IsDecimal, IsNumber } from 'class-validator';
+import { IsDecimal, IsNumber, IsOptional } from 'class-validator';
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 
 @Entity()
 export class UpdateentradaDto {
-  @PrimaryGeneratedColumn()
+  @IsOptional()
+  @Column()
   nombre?: string;
 
+  @IsOptional()
   @Column({ type: 'decimal', precision: 10, scale: 2 })
   precio: number;
 
+  @IsOptional()
   @Column()
   tipo?: string;
 
+  @IsOptional()
   @Column()
   descripcion?: string;
 
+  @IsOptional()
   @Column({ nullable: true })
   Porciones: string;
 
+  @IsOptional()
   @Column({ default: true })
   disponibilidad: boolean;
 }
