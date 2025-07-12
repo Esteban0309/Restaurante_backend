58bdfe1df5961bf3d5cb6ac16bfcec34b6b70dda
Esteban0309 <corellaesteban_11@hotmail.com>
2025-07-12 09:34:33 -0500

    ACTU

--- env.tsx
+++ env.tsx
@@ -1,8 +1,8 @@
-// DB_HOST=localhost
-// DB_PORT=5432
-// DB_USERNAME=postgres
-// DB_PASSWORD=postgres
-// DB_NAME=neondb
+//DB_HOST=ep-falling-forest-aeid1zcl-pooler.c-2.us-east-2.aws.neon.tech
+//DB_PORT=5432
+//DB_USER=neondb_owner
+//DB_PASS=npg_tWhZd53vzKAp
+//DB_NAME=neondb
 
 // # MONGO_URI=mongodb+srv://estebancorella11:Pulguin11@clusteresteban.dniyljx.mongodb.net/restaurante?retryWrite=true&w=majority
 // MONGO_URI=mongodb://localhost:27017/restaurante
