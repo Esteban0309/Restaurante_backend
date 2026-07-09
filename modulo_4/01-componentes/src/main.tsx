400d855fb842a50a2ef0c47f83630c61ba775249
Esteban0309 <corellaesteban_11@hotmail.com>
2026-07-08 20:03:54 -0500

    08-07-2026

--- modulo_4/01-componentes/src/main.tsx
+++ modulo_4/01-componentes/src/main.tsx
@@ -1,10 +1,18 @@
+// src/main.tsx
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from './Context/ThemeContext.tsx'
+import { AuthProvider }  from './Context/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
